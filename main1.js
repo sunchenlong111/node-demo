e79209a1c4202646e06d2905dea597a78f159cd4
sunchenlong111 <1094584419@qq.com>
2018-10-24 20:46:59 +0800

    promise初使用

--- main1.js
+++ main1.js
@@ -1,74 +1,73 @@
-window.jQuery = function (nodeOrSelector) {
-  let nodes = {}
-  nodes.addClass = function () {}
-  nodes.html = function () {}
-  return nodes
-}
-window.$ = window.jQuery
+// window.jQuery = function (nodeOrSelector) {
+//   let nodes = {}
+//   nodes.addClass = function () {}
+//   nodes.html = function () {}
+//   return nodes
+// }
+// window.$ = window.jQuery
 
 
-window.jQuery.ajax = function ({url,method,body,successFn,failFn,headers}) {
+// window.jQuery.ajax = function ({url,method,body,successFn,failFn,headers}) {
 
   
-  // let url = options.url
-  // let method = options.method
-  // let body = options.body
-  // let successFn = options.successFn
-  // let failFn = options.failFn
-  // let headers = options.headers
-  // //es6解构赋值
-  // let {url,method,body,successFn,failFn,headers} = options
+//   // let url = options.url
+//   // let method = options.method
+//   // let body = options.body
+//   // let successFn = options.successFn
+//   // let failFn = options.failFn
+//   // let headers = options.headers
+//   // //es6解构赋值
+//   // let {url,method,body,successFn,failFn,headers} = options
 
-   // if(arguments.length === 1){
-   //   url = options.url
-   // }else if(arguments.length === 2){
-   //   url = arguments[0]
-   //   options = arguments[1]
-   // }
-   //如何设置接受两种参数
+//    // if(arguments.length === 1){
+//    //   url = options.url
+//    // }else if(arguments.length === 2){
+//    //   url = arguments[0]
+//    //   options = arguments[1]
+//    // }
+//    //如何设置接受两种参数
 
-  let request = new XMLHttpRequest()
-  request.open(method, url)
+//   let request = new XMLHttpRequest()
+//   request.open(method, url)
   
-  //设置请求头
-  for(let key in headers){
-    let value = headers[key]
-    request.setRequestHeader(key,value)
-  }
+//   //设置请求头
+//   for(let key in headers){
+//     let value = headers[key]
+//     request.setRequestHeader(key,value)
+//   }
 
-  request.onreadystatechange = () => {
-    if (request.readyState === 4) {
-      if (request.status >= 200 && request.status < 300) {
-        successFn.call(undefined, request.responseText)
-      } else if (request.status >= 400) {
-        failFn.call(undefined, request)
-      }
-    }
-  }
-  request.send(body)
-}
+//   request.onreadystatechange = () => {
+//     if (request.readyState === 4) {
+//       if (request.status >= 200 && request.status < 300) {
+//         successFn.call(undefined, request.responseText)
+//       } else if (request.status >= 400) {
+//         failFn.call(undefined, request)
+//       }
+//     }
+//   }
+//   request.send(body)
+// }
 
-function f1(responseText){}
-function f2(responseText){}
+// function success(responseText){
+//   console.log(responseText)
+// }
+// function fail(request){
+//   console.log(request)
+// }
 
 btn.addEventListener('click', (e) => {
-  
-    window.jQuery.ajax({
+    $.ajax({
       url: '/xxx',
-      method: 'get',
-      headers:{
-        'Content-type':'application/x-www-form-urlencoded',
-        'frank':'18'
-      },
-      successFn: (x) => {
-        console.log(x)
-        f1.call(undefined,x)
-        f2.call(undefined,x)
-      },
-      failFn: (x) => {
-        console.log(x)
-        console.log(x.status)
-        console.log(x.responseText)
-      }
+      type: 'get',
+    }).then((responseText)=>{
+      console.log(responseText)
+      return responseText
+    },(request)=>{
+      console.log(request)
+      return '已经处理'
+    }).then((上一次处理的结果)=>{
+      console.log(上一次处理的结果)//上一次处理的结果就是第一个then返回的responseText
+    },(request)=>{
+      console.log(request)
     })
 })
